fb40c71b2e993832be14c0734c13cd432170476c
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-10-13 20:20:31 +0200

    updates and styles added t active state

--- static-jobs-listing/src/component/ActiveState.js
+++ static-jobs-listing/src/component/ActiveState.js
@@ -3,7 +3,17 @@ import React from 'react'
 const ActiveState = () => {
   return (
     <div className='max-w-[75vw] left-[12%] my-[4.5rem] absolute top-[2.5%] w-full rounded-md px-8 py-4 bg-white flex justify-between items-center'>
-        <button>Hello</button>
+        <div className='flex gap-4'>
+            <div className='active flex gap-2'>
+                <p className='active-p'>hello</p>
+                <span className='active-span'><i class="fa-solid fa-xmark" style={{fontSize: '1.5rem'}}></i></span>
+            </div>
+            <div className='active flex gap-2'>
+                <p className='active-p'>Javascript</p>
+                <span className='active-span'><i class="fa-solid fa-xmark" style={{fontSize: '1.5rem'}}></i></span>
+            </div>
+        </div>
+        
         <button className='text-darkCyan capitalize font-medium hover:underline'>clear</button>
     </div>
   )
